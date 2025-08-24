675d76f536b4766ae17e019bc108a6091b04eddf
gumoura82 <gumoura82@gmail.com>
2025-08-24 19:40:54 -0300

    chore: adicionado Tailwind e pequenos ajustes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter ([
+  {
+    path:'/',
+    element: <App />,
+    children: [
+      {index: true, element: <Home />},
+      {path: 'tecnica', element: <Tecnica />},
+      {path: 'contato', element: <Contato />},
+      {path: '*', element: <PageNotFound />}
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
